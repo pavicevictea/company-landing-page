748be7be86094f789b925c2a864f94e384389d51
Pavicevic Tea <pavicevictea6@gmail.com>
2026-09-07 16:37:07 +0200

    Contact form validation added

--- company-landing-page/src/app/components/contact/contact.component.ts
+++ company-landing-page/src/app/components/contact/contact.component.ts
@@ -7,14 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onNameInput(event: any) {
+    this.name = event.target.value;
+  }
+
+  onEmailInput(event: any) {
+    this.email = event.target.value;
+  }
+
+  onMessageInput(event: any) {
+    this.message = event.target.value;
+  }
+
+  get isFormValid(): boolean {
+    return this.name.trim() !== '' && this.email.trim() !== '' && this.message.trim() !== '';
+  }
+
   showMessage(event: Event) {
     event.preventDefault();
+    if (!this.isFormValid) return;
+    
     alert('This is a demo contact form. No message will be sent');
+    this.name = '';
+    this.email = '';
+    this.message = '';
   }
 
 }
