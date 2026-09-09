38de02b1c8b4a87cc32a9c2dac856427d9a6f288
Pavicevic Tea <pavicevictea6@gmail.com>
2026-09-09 02:00:50 +0200

    Files organized and backend project added

---
+++ backend/company-landing-page/src/main/java/com/example/companylandingpage/CompanyLandingPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.companylandingpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyLandingPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyLandingPageApplication.class, args);
+    }
+
+}
---
+++ backend/company-landing-page/src/test/java/com/example/companylandingpage/CompanyLandingPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companylandingpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyLandingPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
