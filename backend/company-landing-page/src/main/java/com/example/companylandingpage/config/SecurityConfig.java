04c975f1447a71b47da2402fd9f27847b8bfc656
Pavicevic Tea <pavicevictea6@gmail.com>
2026-09-13 18:58:59 +0200

    feat: admin authorization added to API

--- backend/company-landing-page/src/main/java/com/example/companylandingpage/config/SecurityConfig.java
+++ backend/company-landing-page/src/main/java/com/example/companylandingpage/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.example.companylandingpage.repository.AdminUserRepository;
 import org.hibernate.mapping.UserDefinedArrayType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -26,5 +28,33 @@ public class SecurityConfig {
                         .roles(user.getRole())
                         .build())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/contact", "/api/auth/**","/h2-console/**")
+                .permitAll()
+                .requestMatchers("/api/content/**").hasRole("ADMIN")
+                .anyRequest()
+                .permitAll()
+            ).formLogin(form -> form
+                .loginProcessingUrl("/api/auth/login")
+                .successHandler((request, response, authentication) -> {
+                    response.setStatus(200);
+                })
+                .failureHandler((request, response, exception) -> {
+                    response.setStatus(401);
+                })
+                .permitAll()
+            ).logout(logout -> logout
+                .logoutUrl("/api/auth/logout")
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.setStatus(200);
+                })
+                .permitAll()
+            ).csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
