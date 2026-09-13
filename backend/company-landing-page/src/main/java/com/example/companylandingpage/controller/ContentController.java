26e6cbdcf70a32fc136fe3829e0e1e511340b754
Pavicevic Tea <pavicevictea6@gmail.com>
2026-09-13 20:45:18 +0200

    feat: added controller for crud

---
+++ backend/company-landing-page/src/main/java/com/example/companylandingpage/controller/ContentController.java
@@ -0,0 +1,47 @@
+package com.example.companylandingpage.controller;
+
+import com.example.companylandingpage.model.ContentItem;
+import com.example.companylandingpage.service.ContentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/content")
+@CrossOrigin(origins = "http://localhost:4000", allowCredentials = "true")
+public class ContentController {
+    private final ContentService contentService;
+
+    public ContentController(ContentService contentService) {
+        this.contentService = contentService;
+    }
+
+    @GetMapping
+    public List<ContentItem> getAll() {
+        return contentService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ContentItem getById(@PathVariable Long id) {
+        return contentService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContentItem create(@Valid @RequestBody ContentItem contentItem) {
+        return  contentService.create(contentItem);
+    }
+
+    @PutMapping("/id")
+    public ContentItem update(@PathVariable Long id, @Valid @RequestBody ContentItem contentItem) {
+        return contentService.update(id, contentItem);
+    }
+
+    @DeleteMapping("/id")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        contentService.delete(id);
+    }
+}
