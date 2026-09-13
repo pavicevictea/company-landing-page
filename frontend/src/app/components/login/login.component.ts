21f00affd3b4cf89994ba11ca0580c39b7bcf121
Pavicevic Tea <pavicevictea6@gmail.com>
2026-09-13 21:41:08 +0200

    ui: login page added

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = ''
+  password = '';
+  errorMessage = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+  }
+
+  login(event: Event) {
+    event.preventDefault();
+
+    this.errorMessage = '';
+
+    this.authService.login(this.username, this.password)
+      .subscribe(
+        () => {
+          this.router.navigate(['/admin/content']);
+        },
+        () => {
+          this.errorMessage = 'Invalid username or password.';
+        }
+      );
+  }
+
+}
