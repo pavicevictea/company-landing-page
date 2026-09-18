6e94f192c76ad102652d4deb80dbf2a0a8128761
Pavicevic Tea <pavicevictea6@gmail.com>
2026-09-18 02:35:42 +0200

    ui: added needed service file to frontend

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface UserProfile {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+export interface UserUpdateRequest {
+  name: string;
+  email: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'http://localhost:8080/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getCurrentUser(): Observable<UserProfile> {
+    return this.http.get<UserProfile>(
+      `${this.apiUrl}/me`,
+      {
+        withCredentials: true
+      }
+    );
+  }
+
+  updateCurrentUser(
+    user: UserUpdateRequest
+  ): Observable<UserProfile> {
+    return this.http.put<UserProfile>(
+      `${this.apiUrl}/me`,
+      user,
+      {
+        withCredentials: true
+      }
+    );
+  }
+}
