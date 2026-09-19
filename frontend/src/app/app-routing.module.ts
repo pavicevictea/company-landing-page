e800293d9d3bb62ba2e12098640c7a75d808d102
Pavicevic Tea <pavicevictea6@gmail.com>
2026-09-19 06:27:24 +0200

    ui: routing added for user dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { ContentManagementComponent } from './components/content-management/cont
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin/content', component: ContentManagementComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent},
+  { path: 'dashboard', component: UserDashboardComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
 
